6f71767898229c1ac111884ed8d02c7d1bc470d6
ivoferroo <1151159@isep.ipp.pt>
2016-11-29 16:38:48 +0000

    implements view 1 [implementation]

---
+++ scripts/views.sql
@@ -0,0 +1,24 @@
+-- VIEWS
+
+-- 1.
+-- View que permita obter os pilotos que mais horas fizeram de voo por tipo de
+-- avião (tipo avião, código piloto, nome, número horas)
+
+CREATE OR REPLACE VIEW VW_HORAS_VOO_POR_AVIAO AS
+
+SELECT Ma.NOME marca, MM.NOME modelo, HV.PILOTO_ID id_piloto, T.NOME nome_piloto, HV.HORAS_VOO horas_voo
+
+FROM MARCA Ma, MARCA_MODELO MM, TRIPULANTE T, HORAS_VOO HV,
+
+    (SELECT MARCA_MODELO, MAX(HORAS_VOO) HORAS_MAXIMAS
+    FROM HORAS_VOO
+    GROUP BY MARCA_MODELO) SUBQ
+
+WHERE HV.PILOTO_ID = T.TRIPULANTE_ID
+    AND HV.MARCA_MODELO = MM.MARCA_MODELO_ID
+    AND MM.MARCA_ID = Ma.MARCA_ID
+    AND HV.MARCA_MODELO = SUBQ.MARCA_MODELO
+    AND HV.HORAS_VOO = SUBQ.HORAS_MAXIMAS;
+    
+
+SELECT * FROM VW_HORAS_VOO_POR_AVIAO;
