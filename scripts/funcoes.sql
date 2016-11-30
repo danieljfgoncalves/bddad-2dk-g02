1cbf2153900b42f2621a365086ecfd6f651280c3
Daniel Gonçalves <1151452@isep.ipp.pt>
2016-11-30 09:57:47 +0000

    creates funcoes script

---
+++ scripts/funcoes.sql
@@ -0,0 +1,18 @@
+-- Funções
+-- 4. 
+-- Função que para um voo de ligação entre dois aeroportos devolva 
+-- o código do voo regular com o preço mais baixo dessa ligação para uma dada classe.
+
+
+
+-- 5. 
+-- Função que devolva o salário total de um piloto num dado mês.
+
+
+
+
+-- 6. 
+-- Função que devolva o número de um avião que pode ser alocado a um dado voo regular. 
+-- Um avião só pode ser alocado a um voo se o aeroporto origem corresponde ao aeroporto 
+-- destino da última viagem que o avião faz (onde o avião se encontra) e a hora de partida é 
+-- superior a 2 h em relação ao tempo de chegada desta última viagem.
