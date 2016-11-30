a64757d5eefbfaac4ddf7910a130732f68ab7a44
Daniel Gonçalves <1151452@isep.ipp.pt>
2016-11-30 00:11:28 +0000

    update views & insert_data scripts

--- scripts/views.sql
+++ scripts/views.sql
@@ -2,7 +2,7 @@
 
 -- 1.
 -- View que permita obter os pilotos que mais horas fizeram de voo por tipo de
--- avião (tipo avião, código piloto, nome, número horas)
+-- aviï¿½o (tipo aviï¿½o, cï¿½digo piloto, nome, nï¿½mero horas)
 
 CREATE OR REPLACE VIEW VW_HORAS_VOO_POR_AVIAO AS
 
@@ -22,29 +22,29 @@ WHERE HV.PILOTO_ID = T.TRIPULANTE_ID
 
 -- 2.
 -- View que permita obter todos os voos regulares que se realizam 
--- entre duas cidades para as quais há ligações todos os dias da 
--- semana (cidade origem, cidade destino, dia semana, horário).
+-- entre duas cidades para as quais hï¿½ ligaï¿½ï¿½es todos os dias da 
+-- semana (cidade origem, cidade destino, dia semana, horï¿½rio).
 
-CREATE OR REPLACE VIEW VW_VOOS_TODOS_DIAS AS
+CREATE OR REPLACE VIEW VW_VOOS_TD_SEMANA AS
 
-SELECT CO.NOME, CD.NOME, VR.DIA_DA_SEMANA, VR.HORARIO_PARTIDA
+SELECT  CO.NOME ORIGEM, CD.NOME DESTINO, VR.DIA_DA_SEMANA, VR.HORARIO_PARTIDA
 FROM    CIDADE CO, CIDADE CD, AEROPORTO AO, AEROPORTO AD, VOO V, VOO_REGULAR VR,
         (
           SELECT VR2.VOO_ID
           FROM VOO_REGULAR VR2
           GROUP BY VR2.VOO_ID
           HAVING COUNT(DISTINCT VR2.DIA_DA_SEMANA) = 7
-        ) VOO_TODOS_DIAS
+        ) VOO_TDS_DIAS
 WHERE   VR.VOO_ID = V.VOO_ID
     AND V.AEROPORTO_ORIGEM = AO.IATA_ID
     AND V.AEROPORTO_DESTINO = AD.IATA_ID
     AND AO.CIDADE_ID = CO.CIDADE_ID
     AND AD.CIDADE_ID = CD.CIDADE_ID
-    AND VOO_TODOS_DIAS.VOO_ID = VR.VOO_ID
+    AND VOO_TDS_DIAS.VOO_ID = VR.VOO_ID
 ORDER BY DIA_DA_SEMANA;
 
 -- 3.
--- View que para cada voo de ligação indique a viagem com maior atraso (código
+-- View que para cada voo de ligaï¿½ï¿½o indique a viagem com maior atraso (cï¿½digo
 -- voo_regular, data, aeroporto origem, aeroporto destino, tempo de atraso).
 
 -- coisas que testei
