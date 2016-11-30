0b36c4ee2fa45372e0da30e94db2cd90163f2625
Daniel Gonçalves <1151452@isep.ipp.pt>
2016-11-30 15:55:23 +0000

    creates the procedures script

---
+++ scripts/procedures.sql
@@ -0,0 +1,6 @@
+-- Procedures
+-- 8. 
+-- Procedimento que atribua um avião e a tripulação a cada voo regular para um período. 
+-- Considere que a tripulação e o avião são sempre os mesmos para todas as viagens que 
+-- se realizam nesse período correspondentes ao mesmo voo_regular.
+
